4963664f1378efb492438a6dbfa30147fe084d5d
Glen Mora <glen.mora@sarasotacountyschools.net>
2020-10-13 11:46:33 -0400

    Update DoThis.py

--- DoThis.py
+++ DoThis.py
@@ -1,1 +1,1 @@
-# in this file, print your name followed by pi to the 5th power, then push it up to github.
+# in this file, print your name followed by pi to the 5th power, then commit and push it back up to github.
