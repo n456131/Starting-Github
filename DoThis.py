01b5247fead1a12263a6dec3c63806e94d2465cb
Filipe <73903029+n456131@users.noreply.github.com>
2020-11-06 12:20:04 -0500

    Update DoThis.py

--- DoThis.py
+++ DoThis.py
@@ -3,3 +3,12 @@
 # pi to the power of the first number 
 # or the square root of the second number.
 # then commit and push it back up to github.
+
+import math
+print("Filipe Siqueira")
+Num1 = int(input("Enter a number: "))
+Num2 = int(input("Enter a number: "))
+if ((math.pow(π,Num1)) > math.sqrt(Num2)):
+  print(math.pow(π,Num1))
+else:
+  print(math.sqrt(Num2))
